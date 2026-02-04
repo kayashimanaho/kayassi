8022b0a5c3992995bc1991b45b37b85156184202
萱島菜穂 <caisuiz839@gmail.com>
2026-02-04 22:11:35 +0900

    edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+
+
+
       <body
         className={`${notoSans.variable} ${notoSerif.variable} font-[family-name:var(--font-noto-sans)] antialiased`}
       >
